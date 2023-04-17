27822a8e0fc32488d1085a435a7a4ef8575d7f53
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-04-17 13:25:07 -0400

    Adding data sample

---
+++ AdventureTrips/data.js
@@ -0,0 +1,31 @@
+const data = [
+    {
+        title: "Orlando Geocaching",
+        date: "01/14/2023",
+        location: "UCF Arboretum",
+        type: "Day Trip",
+        description: "Ex do irure ex do. Amet incididunt officia aliquip ea occaecat qui nisi cillum officia labore qui duis voluptate. Qui esse amet dolore laboris non id nulla sint tempor aliqua non consectetur ipsum.Anim ipsum occaecat cupidatat laboris dolore dolore veniam duis minim irure sint.",
+        saved: false,
+        tripLeaders: "Polina, Richard",
+        cost: 5,
+        regOpen: "01/03/2023, 12PM",
+        regClosed: "01/11/2023, 5PM",
+        preTrip: "01/11/2023, 6PM"
+    },
+    {
+        title: "Caving and Camping",
+        date: "01/14/2023 - 01/16/2023",
+        location: "Tumbling Rock Cave, AL",
+        type: "Overnight Trip",
+        description: "Ex do irure ex do. Amet incididunt officia aliquip ea occaecat qui nisi cillum officia labore qui duis voluptate. Qui esse amet dolore laboris non id nulla sint tempor aliqua non consectetur ipsum.Anim ipsum occaecat cupidatat laboris dolore dolore veniam duis minim irure sint.",
+        saved: false,
+        tripLeaders: "Kristen, Brandi, Cody",
+        cost: 50,
+        regOpen: "01/03/2023, 12PM",
+        regClosed: "01/10/2023, 5PM",
+        preTrip: "01/10/2023, 6PM"
+    }
+
+]
+
+export default data;
