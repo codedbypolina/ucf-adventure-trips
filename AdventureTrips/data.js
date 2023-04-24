791283a57b576876be3df2a85814b4da0f72fc0c
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-04-24 13:35:51 -0400

    Adding more data

--- AdventureTrips/data.js
+++ AdventureTrips/data.js
@@ -43,13 +43,88 @@ const data = [
         description: "Ex do irure ex do. Amet incididunt officia aliquip ea occaecat qui nisi cillum officia labore qui duis voluptate. Qui esse amet dolore laboris non id nulla sint tempor aliqua non consectetur ipsum.Anim ipsum occaecat cupidatat laboris dolore dolore veniam duis minim irure sint.",
         saved: false,
         tripLeaders: "Kristen, Brandi, Cody",
-        cost: 50,
+        cost: 20,
         regOpen: "01/03/2023, 12PM",
         regClosed: "01/10/2023, 5PM",
         preTrip: "01/10/2023, 6PM",
         id: 30
-    }
+    },
+    {
+        title: "Paddle with Pride",
+        image: "https://iili.io/H8uVshN.md.png",
+        date: "01/25/2023-01/26/2023",
+        location: "Banana River",
+        emoji: "🏕",
+        type: "Overnight Trips",
+        description: "Ex do irure ex do. Amet incididunt officia aliquip ea occaecat qui nisi cillum officia labore qui duis voluptate. Qui esse amet dolore laboris non id nulla sint tempor aliqua non consectetur ipsum.Anim ipsum occaecat cupidatat laboris dolore dolore veniam duis minim irure sint.",
+        saved: true,
+        tripLeaders: "Polina, Richard",
+        cost: 30,
+        regOpen: "01/15/2023, 12PM",
+        regClosed: "01/20/2023, 5PM",
+        preTrip: "01/20/2023, 6PM",
+        registration: true,
+        id: 40
+
+    },
+    {
+        title: "Bike & Beans",
+        image: "https://iili.io/H8uhxLB.md.png",
+        date: "01/25/2023",
+        location: "UCF Arboretum",
+        emoji: "🚴",
+        type: "Biking Trips",
+        description: "Ex do irure ex do. Amet incididunt officia aliquip ea occaecat qui nisi cillum officia labore qui duis voluptate. Qui esse amet dolore laboris non id nulla sint tempor aliqua non consectetur ipsum.Anim ipsum occaecat cupidatat laboris dolore dolore veniam duis minim irure sint.",
+        saved: true,
+        tripLeaders: "Polina, Richard",
+        cost: 5,
+        regOpen: "01/18/2023, 12PM",
+        regClosed: "01/21/2023, 5PM",
+        preTrip: "01/21/2023, 6PM",
+        registration: true,
+        id: 50
+
+    },
+
+    {
+        title: "Backpacking",
+        image: "https://iili.io/H8uwoKP.md.png",
+        date: "02/10/2023",
+        location: "Cape Canaveral",
+        emoji: "🥾",
+        type: "Hiking Trips",
+        description: "Ex do irure ex do. Amet incididunt officia aliquip ea occaecat qui nisi cillum officia labore qui duis voluptate. Qui esse amet dolore laboris non id nulla sint tempor aliqua non consectetur ipsum.Anim ipsum occaecat cupidatat laboris dolore dolore veniam duis minim irure sint.",
+        saved: false,
+        tripLeaders: "Polina, Richard",
+        cost: 15,
+        regOpen: "02/02/2023, 12PM",
+        regClosed: "02/05/2023, 5PM",
+        preTrip: "02/05/2023, 6PM",
+        registration: true,
+        id: 60
+
+    },
+
+    {
+        title: "Goat Yoga",
+        image: "https://iili.io/H8uO8tj.md.png",
+        date: "02/20/2023",
+        location: "Florida",
+        emoji: "🧘",
+        type: "Stress Relief Trips",
+        description: "Ex do irure ex do. Amet incididunt officia aliquip ea occaecat qui nisi cillum officia labore qui duis voluptate. Qui esse amet dolore laboris non id nulla sint tempor aliqua non consectetur ipsum.Anim ipsum occaecat cupidatat laboris dolore dolore veniam duis minim irure sint.",
+        saved: false,
+        tripLeaders: "Polina, Richard",
+        cost: 5,
+        regOpen: "02/13/2023, 12PM",
+        regClosed: "02/16/2023, 5PM",
+        preTrip: "02/16/2023, 6PM",
+        registration: true,
+        id: 70
+
+    },
 
 ]
 
 export default data;
+
